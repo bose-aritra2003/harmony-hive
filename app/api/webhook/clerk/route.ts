68d8402fae74cc812f5db140da7987cafc36c37e
Aritra Bose <aritrabose2003@gmail.com>
2024-04-05 11:59:34 +0530

    debug

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -66,7 +66,9 @@ export async function POST(req: Request) {
       photo: image_url,
     }
 
+    console.log(user)
     const newUser = await createUser(user);
+    console.log(newUser)
 
     if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
