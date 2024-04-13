f774a09c440b9ec5efe57636ed44aa887bd49142
Aritra Bose <aritrabose2003@gmail.com>
2024-04-13 20:42:17 +0530

    fix clerk sync with mongodb

--- middleware.ts
+++ middleware.ts
@@ -2,31 +2,29 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: [
-    "/",
-    "/favicon.ico",
-    "events/:id",
-    "/api/webhook(.*)",
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/uploadthing",
-    "/assets/images/logo.svg",
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+    '/assets/images/logo.svg',
     "/assets/images/hero.png",
     "/assets/images/dotted-pattern.png",
-    "/sign-in"
+    'fabicon.ico',
+    '/sign-in'
   ],
   ignoredRoutes: [
-    "/favicon.ico",
-    "/api/webhook(.*)",
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/uploadthing",
-    "/assets/images/logo.svg",
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+    '/assets/images/logo.svg',
     "/assets/images/hero.png",
     "/assets/images/dotted-pattern.png",
-    "/sign-in"
+    '/fabicon.ico',
+    '/sign-in'
   ]
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).)", "/", "/(api|trpc)(.)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
